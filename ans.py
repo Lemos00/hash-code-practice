568f14d698d86350e3d5eb024c6a62385a4f7ce8
Zac <zbennet2@uwo.ca>
2022-02-18 21:51:01 -0500

    File input and unique ingredients

--- ans.py
+++ ans.py
@@ -12,27 +12,36 @@
 #   score ingredients
 #   score ppl
 # 4 cheese tomato pineapple pepper
+from collections import Counter
 ingredients = {}
 people = {}
 # 2 mushrooms tomatoes
 def main():
-  global people, ingredients
-  with open('a_an_example.in.txt') as f:
-      lines = f.readlines()
+    global people, ingredients
+    with open('test-cases/a_an_example.in.txt') as f:
+        lines = f.readlines()
 
-  playerNumber=1
-  
-  for i in range(int(lines[0])):
-      line = lines[playerNumber].split(" ")
-      for j in range(1,len(line)):
-          print(line[j],end="")
+    playerNumber=1
+    likes = []
+    dislikes=[]
+    for i in range(int(lines[0])):
 
-      playerNumber+=1
-      line =lines[playerNumber].split(" ")
-      for j in range(1,len(line)):
-          print(line[j],end="")
-      playerNumber+=1
-    
-    
+        line = lines[playerNumber].split(" ")
+        for j in range(1,len(line)):
+            #adding the likes and removing the \n on the final value
+            likes.append(line[j] if j==line[-1] else line[j].rstrip("\n"))
+
+        playerNumber+=1
+        line =lines[playerNumber].split(" ")
+        for j in range(1,len(line)):
+            #adding the dislikes and removing the \n on the final valueF
+            dislikes.append(line[j] if j==line[-1] else line[j].rstrip("\n"))
+
+        playerNumber+=1
+    print(Counter(likes),Counter(dislikes))
+    releventIngredients= list(set(likes+dislikes))
+    print(releventIngredients)
+
+
 if __name__ == "__main__":
     main()
